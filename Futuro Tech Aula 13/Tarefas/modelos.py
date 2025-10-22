b83f678ccb557962237d3901ac412d5618076bc1
fernanddadantas <fernandamoreira079@gmail.com>
2025-10-22 10:31:32 -0300

    Futuro Tech

--- Futuro Tech Aula 13/Tarefas/modelos.py
+++ Futuro Tech Aula 13/Tarefas/modelos.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Apenas cria o objeto (SQLAlchemy), sem vinculá-lo ainda
 db = SQLAlchemy()  
@@ -10,14 +11,20 @@ class Tarefas(db.Model):
     descricao = db.Column(db.Text, nullable=True)
     # Usa Boolean para representar se a tarefa foi concluída
     concluida = db.Column(db.Boolean, nullable=False, default=False)
-    data_criacao = db.Column(db.DateTime, default=db.func.now())
-    prioridade = db.Column(db.String(20), default='normal')  # 'baixa', 'normal', 'alta'
+    data_criacao = db.Column(db.DateTime, default=datetime, nullable=False)
+    prioridade = db.Column(db.String(20), default='media')  # 'baixa', 'media', 'alta'
 
-    def __init__(self, titulo: str, descricao: str | None = None, concluida: bool = False):
+    def __init__(self, titulo: str, descricao: str | None = None, concluida: bool = False, prioridade: str = 'media') -> None:
         """Construtor explícito para ajudar o type-checker e facilitar
         criação de instâncias com kwargs (titulo, descricao, concluida).
         """
         self.titulo = titulo
         self.descricao = descricao
         self.concluida = concluida
-        
+        self.prioridade = prioridade
+        self.data_criacao = db.func.now()
+
+
+
+
+
